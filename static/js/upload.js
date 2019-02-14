8c07b6655c0daed616be4e88ed08c9576a7c8ff8
梁川将和 <yanagawamasakazu@yanagawama.local>
2019-02-14 11:12:07 +0900

    アップロード改修

--- static/js/upload.js
+++ static/js/upload.js
@@ -2,6 +2,10 @@ $(function () {
     var val = $(".photo_form").val();
     if(!val){
         $(".data_submit_mask").addClass("mask_open");
+        $(".modal_data_submit_mask").addClass("mask_open");
+    } else {
+        $(".data_submit_mask").removeClass("mask_open");
+        $(".modal_data_submit_mask").removeClass("mask_open");
     }
 })
 
@@ -10,10 +14,10 @@ var file = $(".uploadimg").change(function () {
 });
 
 var file = $(".modal_uploadimg").change(function () {
-    $(".data_submit_mask").removeClass("mask_open");
+    $(".modal_data_submit_mask").removeClass("mask_open");
 });
 
-$(".photo_form").bind("change keyup",function(){
+$(".photo_form").bind("keyup",function(){
     var count = $(this).val().length;
     count = 300 - count;
     if(count < 0) {
@@ -21,16 +25,12 @@ $(".photo_form").bind("change keyup",function(){
         $(".data_submit_mask").addClass("mask_open");
     } else {
         $(".text_count").css('color', 'black');
-        if(file){
-            $(".data_submit_mask").removeClass("mask_open");
-        }
-        
     }
     $(".text_count").text(count);
     
 });
 
-$(".modal_photo_form").bind("change keyup",function(){
+$(".modal_photo_form").bind("keyup",function(){
     $(".modal_text_count").text("300");
     var count = $(this).val().length;
     count = 300 - count;
@@ -38,11 +38,7 @@ $(".modal_photo_form").bind("change keyup",function(){
         $(".text_count").css('color', 'red');
         $(".data_submit_mask").addClass("mask_open");
     } else {
-        $(".text_count").css('color', 'black');
-        if(file){
-            $(".data_submit_mask").removeClass("mask_open");
-        }
-        
+        $(".text_count").css('color', 'black'); 
     }
 
     $(".modal_text_count").text(count);
@@ -56,9 +52,9 @@ $('.uploadimg').change(function(){
         var reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function() {
-            $('.preview').attr('src', reader.result );
-            $('.preview').addClass('cover');
-            $(".preview").removeClass("mask_open");
+            $('#preview').attr('src', reader.result );
+            $('#preview').addClass('cover');
+            $("#preview").removeClass("mask_open");
         }
     }
 });
@@ -70,9 +66,9 @@ $('.modal_uploadimg').change(function(){
         reader.readAsDataURL(file);
         
         reader.onload = function() {
-            $('.modal_preview').attr('src', reader.result );
-            $('.modal_preview').addClass('cover');
-            $(".modal_preview").removeClass("mask_open");
+            $('#modal_preview').attr('src', reader.result );
+            $('#modal_preview').addClass('cover');
+            $("#modal_preview").removeClass("mask_open");
         }
     }
 });
