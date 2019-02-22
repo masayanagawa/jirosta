e3775d87c1fa62dde39a167c5309c6e935e1c48d
梁川将和 <yanagawamasakazu@yanagawama.local>
2019-02-22 20:57:18 +0900

    settings edit

--- jiro/settings.py
+++ jiro/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/')
 MEDIA_URL = 'static/img/'
