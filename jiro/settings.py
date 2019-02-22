da11e1775d791f8732c95c4633ed3e8464f7be27
梁川将和 <yanagawamasakazu@yanagawama.local>
2019-02-22 20:12:02 +0900

    settings.py edit

--- jiro/settings.py
+++ jiro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    import django_heroku
     django_heroku.settings(locals())
 
 ALLOWED_HOSTS = []
